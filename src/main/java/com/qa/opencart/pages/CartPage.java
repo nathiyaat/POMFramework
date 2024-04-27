0795f28dd3c6d0cd2a3349c3df6a65b4e6e88777
Nathiya Satish <nathiasatish@gmail.com>
2024-04-27 19:58:59 +0400

    updated cart page

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 public class CartPage {
 	int i=10;
 	
-	private By name =By.name("testing");
+	private By name =By.name("test");
 	
 	public void getName() {
 		System.out.println("Name is ");
