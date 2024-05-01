06f11ae548ca9f8363ff8f628ea3683f9eab730e
Nathiya Satish <nathiasatish@gmail.com>
2024-05-01 21:07:07 +0400

    renamed jenkins file

--- src/main/java/com/qa/opencart/constants/AppConstants.java
+++ src/main/java/com/qa/opencart/constants/AppConstants.java
@@ -3,7 +3,7 @@ package com.qa.opencart.constants;
 public class AppConstants {
 
 	
-	public static final String LOGIN_PAGE_TITLE="Account Login11";
+	public static final String LOGIN_PAGE_TITLE="Account Login";
 	public static final String ACCOUNT_PAGE_TITLE="My Account";
 	
 	public static final String LOGIN_PAGE_URL_FRACTION="route=account/login";
